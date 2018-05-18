701970ab0dc9a6c4a0ba0621da76be46d4102ff8
Unknown <benjamin_pestoor@hotmail.com>
2018-05-18 09:55:05 +0200

    added arrays

---
+++ arrays.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "script hoorcollege arrays"
+array1=(1 2 4 8 16 32)
+#echo ${#array1[*]}
+#echo ${#array1[@]}
+#echo ${array1[*]}
+echo "----------------"
+for i in "${array1[@]}"
+do
+	echo $i
+done
+echo "----------------"
